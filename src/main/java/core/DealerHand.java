22db1ab202ed3549ffdd013099584c2bc71320df
Murad Berhanu <MuradBehanu@Murads-MacBook-Pro.local>
2018-09-19 00:25:25 -0400

    added DealerHand file and related code

---
+++ src/main/java/core/DealerHand.java
@@ -0,0 +1,38 @@
+package core;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class DealerHand {
+	List<String> cards = new LinkedList<String>();
+	
+	public int numCards() {
+		return cards.size();
+	}
+	
+	
+	public String displayCards() {
+		String cardsString;
+		if (numCards() == 2) {
+			cardsString = cards.get(0);;
+			cardsString = "dealer cards: " + cardsString;
+			System.out.printf(cardsString);
+			System.out.println("\n");
+		}
+		else { //dealer had 3 cards
+			cardsString = String.join(",", cards);
+			cardsString = "dealer cards: " + cardsString;
+			System.out.printf(cardsString);
+			System.out.println("\n");
+		}
+		return cardsString;
+	}
+	
+	public void hit(String card) {
+		cards.add(card);
+	}
+	
+	public void stand() {
+		System.out.println("Dealer Stands \n");
+	}
+}
